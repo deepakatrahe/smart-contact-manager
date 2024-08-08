8c9bb0e306d2eac46b69304e05c973ce6d8d4de0
deepakatrahe <deepakatrahe@gmail.com>
2024-08-08 12:36:11 +0530

    rest_controller

--- src/main/java/com/bng/educationportal/smartcontactmanager/controller/HealthCheckController.java
+++ src/main/java/com/bng/educationportal/smartcontactmanager/controller/HealthCheckController.java
@@ -1,4 +1,7 @@
 package com.bng.educationportal.smartcontactmanager.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheckController {
 }
